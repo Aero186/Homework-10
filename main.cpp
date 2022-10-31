ed4fa853f4e0113236ba6f0915d696fecd13e530
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-31 20:36:36 +0000

    it outputs lose, but not win...

--- main.cpp
+++ main.cpp
@@ -9,30 +9,32 @@ using namespace std;
 
 int main() {
 
-  int n;
   char guess[26];
   char word[16];
-  int row;
   int win = 0;
-  int lose = 0;
+
   
   cin >> word;
-  cout << "Word is: " << word << endl;
+  //cout << "Word is: " << word << endl;
   cin >> guess;
-  cout << "Guess is: " << guess << endl;
+  //cout << "Guess is: " << guess << endl;
 
   for (int i = 0; i <= 15; i++){
     int j = 0;
     while(j <= 25){
-      cout << word[i] << " " << guess [j] << endl;
+    if (word[i] == guess[j+1])
+      cout << "LOSE" << endl;
+      return 39;
+      //cout << word[i] << " " << guess [j] << endl;
     if(word[i] == guess[j]){
-      win++;}
-      cout << "<><><><> " << win << endl;
+      win++;
+      }
+      //cout << "<><><><> " << win << endl;
     j++;
     }
   }
-  if (win > 6)
+  if (win < 10)
     cout << "WIN" << endl;
-  if(win < 6)
+  if(win > 10)
       cout << "LOSE" << endl;
-  }
+}
