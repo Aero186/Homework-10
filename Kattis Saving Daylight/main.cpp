bbb8f43a8848f33c7400cf61af48fab66eeda3f5
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-31 20:59:11 +0000

    it compiles

---
+++ Kattis Saving Daylight/main.cpp
@@ -0,0 +1,21 @@
+// Samuel Peel
+// CSCI 130
+// 10-31-22
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  
+string month;
+int day;
+int year;
+double rise;
+double down;
+
+  cin >> month >> day >> year >> rise >> down;
+
+}
