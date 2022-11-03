3515aad7f19f00c2a35cd5eb758dda3dc71b3a85
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-11-03 01:03:36 +0000

    Ok, fingers crossed this struct is correct

---
+++ Bronson 13.2/main.cpp
@@ -0,0 +1,18 @@
+// Samuel Peel
+// CSCI 130
+// 11-2-22
+#include <iostream>
+
+using namespace std;
+
+struct chart {
+  int carNumber;
+  int milesDriven;
+  int gallonsUsed;
+};
+
+int main() {
+
+  
+
+}
