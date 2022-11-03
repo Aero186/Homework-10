472d76027cb9f4f5583fea11923f153e59a6ed77
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-11-03 01:35:54 +0000

    Lookin good!

--- Bronson 13.2/main.cpp
+++ Bronson 13.2/main.cpp
@@ -17,26 +17,15 @@ int main() {
   
   int list[5][3] = {{25, 1450, 62}, {36, 3240, 136}, {44, 1792, 76}, {52, 2360, 105}, {68, 2114, 67}};
   chart c;
-  
-  for (int i = 0; i < 5; i++){
+
+for (int i = 0; i < 5; i++){
     for (int j = 0; j == 0; j++){
-       c.carNumber = list[i][j];
-      cout << c.carNumber << endl; 
-    }
+      c.carNumber = list[i][j];
+      c.milesDriven = list[i][j+1];
+      c.gallonsUsed = list[i][j+2];
+      cout << c.carNumber << " " << c.milesDriven << " " << c.gallonsUsed << endl; 
+      }
   }
-  for (int i = 0; i < 5; i++){
-    for (int j = 1; j == 1; j++){
-       c.milesDriven = list[i][j];
-      cout << c.milesDriven << endl; 
-    }
-  }
-  for (int i = 0; i < 5; i++){
-    for (int j = 2; j == 2; j++){
-       c.gallonsUsed = list[i][j];
-      cout << c.gallonsUsed<< endl; 
-    }
-  }
-  
   struct chart arr[5]; 
 
 }
