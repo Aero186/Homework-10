73a4881faf3d7080f8f80389a4d8aa419eed6e43
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-11-03 01:39:40 +0000

    Tada!!

--- .ccls-cache/@home@runner@Bronson-132/Bronson 13.2@main.cpp
+++ .ccls-cache/@home@runner@Bronson-132/Bronson 13.2@main.cpp
@@ -18,14 +18,15 @@ int main() {
   int list[5][3] = {{25, 1450, 62}, {36, 3240, 136}, {44, 1792, 76}, {52, 2360, 105}, {68, 2114, 67}};
   chart c;
 
+  cout << "<><> Exercise 5 <><>" << endl;
+
 for (int i = 0; i < 5; i++){
     for (int j = 0; j == 0; j++){
       c.carNumber = list[i][j];
       c.milesDriven = list[i][j+1];
       c.gallonsUsed = list[i][j+2];
-      cout << c.carNumber << " " << c.milesDriven << " " << c.gallonsUsed << endl; 
+      cout << c.carNumber << " | " << c.milesDriven << " | " << c.gallonsUsed << " | " << (c.milesDriven/c.gallonsUsed) << endl; 
       }
   }
-  struct chart arr[5]; 
 
 }
